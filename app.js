ae4965839a4ee1b66c3a1df7a0d4bab2bb818cd0
Marijn Ploeg <marijnploeg@gmail.com>
2021-02-21 21:24:41 +0100

    done for today

--- app.js
+++ app.js
@@ -1,48 +1,21 @@
-function smoothScroll (target, duration) {
-    var target = document.querySelector(target);
-    var targetPosition = target.getBoundingClientRect().top;
-    var startPosition = window.pageYOffset;
-    var distance = targetPosition - startPosition;
-    var startTime = null;
+const navbarLinks = document.querySelectorAll("nav a");
 
-    function animation(currentTime) {
-        if (startTime === null) startTime = currentTime;
-        var timeElapsed = currentTime - startTime;
-        var run = ease(timeElapsed, startPosition, distance, duration)
-        window.scrollTo(0, run);
-        if (timeElapsed < duration) requestAnimationFrame(animation);
-    }
+for(let i=0; i<navbarLinks.length; i++) {
+    navbarLinks[i].addEventListener("click", navbarLinkClick);
+}
 
+function navbarLinkClick(event) {
+    smoothScroll(event); // Call the "smoothScroll" function
+}
 
-    function ease (t, b, c, d) {
-        t /= d/2;
-        if (t < 1) return c/2*t*t + b;
-        t--;
-        return -c/2 * (t*(t-2) - 1) + b;
-    }
-
-    requestAnimationFrame(animation);
-};
-
-let home = document.querySelector('#homeAnchor');
-home.addEventListener('click', function () {
-    smoothScroll('#mainPage', 1000)
-});
-
-let about = document.querySelector('#aboutAnchor');
-about.addEventListener('click', function () {
-    smoothScroll('#aboutPage', 1500)
-});
-
-let recipes = document.querySelector('#recipesAnchor');
-recipes.addEventListener('click', function () {
-    smoothScroll('#recipesPage', 1500)
-});
-let contact = document.querySelector('#contactAnchor');
-contact.addEventListener('click', function () {
-    smoothScroll('#contactPage', 1500)
-});
-
+function smoothScroll(event) {
+  event.preventDefault();
+  const targetId = event.currentTarget.getAttribute("href");
+  window.scrollTo({
+    top: targetId==="#" ? 0 : document.querySelector(targetId).offsetTop,
+    behavior: "smooth"
+  });
+}
 
 function parallax(element, distance, speed) {
     const item = document.querySelector(element);
@@ -59,3 +32,91 @@ window.addEventListener('scroll', function() {
     parallax('.mediumCocktail', window.scrollY, 0.2);
     parallax('.largeCocktail', window.scrollY, 0.8);
 });
+
+//Getting API data
+async function getRandomCocktail() {
+    const response = await axios.get('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+    return response;
+}
+
+let newRandomCocktail = document.querySelector('.newRandomCocktails');
+
+newRandomCocktail.addEventListener('click', () => {
+    for (let i = 0; i < 5; i++) {
+        getRandomCocktail().then(fillCards)
+    }
+})
+
+//Filling card data with API
+let cardList = document.querySelector('.cardList');
+
+window.addEventListener('load', (event) => {
+
+    for (let i = 0; i < 5; i++) {
+        getRandomCocktail().then(fillCards)
+    }
+});
+
+function fillCards(response) {
+
+    let data = response.data.drinks[0];
+
+    let emptyCard = createCard();
+    let trunc = data.strInstructions.substr(0, 127) + "\u2026";
+    emptyCard.category.innerHTML = data.strCategory;
+
+    emptyCard.cocktailName.innerHTML = data.strDrink;
+
+    if (data.strInstructions.length < 150) {
+        emptyCard.cocktailInstructions.innerHTML = data.strInstructions;
+    } else {
+        emptyCard.cocktailInstructions.innerHTML = trunc + `...`
+    }
+    emptyCard.tag.innerHTML = 'Read more';
+    cardList.appendChild(emptyCard.card);
+
+    console.log(cardList);
+}
+
+class cardElement {
+    constructor(card, category, cocktailName, cocktailInstructions, tag, tags) {
+        this.card = card;
+        this.category = category;
+        this.cocktailName = cocktailName;
+        this.cocktailInstructions = cocktailInstructions;
+        this.tag = tag;
+        this.tags = tags;
+    }
+
+    addClasses() {
+        this.card.classList.add('card');
+        this.category.classList.add('category');
+        this.cocktailName.classList.add('cocktailName');
+        this.cocktailInstructions.classList.add('cocktailInstructions');
+        this.tags.classList.add('tags');
+        this.tag.classList.add('tag');
+    }
+
+    appendElements(){
+        this.tags.appendChild(this.tag);
+        this.card.appendChild(this.category);
+        this.card.appendChild(this.cocktailName);
+        this.card.appendChild(this.cocktailInstructions);
+        this.card.appendChild(this.tags);
+    }
+}
+
+function createCard() {
+    let card = document.createElement('article');
+    let category = document.createElement('p');
+    let cocktailName = document.createElement('h1');
+    let cocktailInstructions = document.createElement('p')
+    let tags = document.createElement('a');
+    let tag = document.createElement('div');
+
+    let randomCard = new cardElement(card, category, cocktailName, cocktailInstructions, tags, tag);
+    randomCard.addClasses();
+    randomCard.appendElements();
+    return randomCard;
+}
+
